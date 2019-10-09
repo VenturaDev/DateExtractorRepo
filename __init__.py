3e93f84b9f27a98460a38407225e6f9cc24adb5d
Ventura-Traveset <albert.ventura@maticapartners.com>
2019-10-09 17:38:41 +0200

    changes in parameters

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 import datetime
-def extract_format(date_column):
-    list_date_formats = ["yyyy-mm-dd", "dd-mm-yyyy", "mm/dd/yyyy", "%m/%d/%Y", "%m/%d/%y", "%d/%m/%Y", "%d/%m/%y", "%d-%m-%Y", 
+def extract_format(date_column, list_date_formats = ["yyyy-mm-dd", "dd-mm-yyyy", "mm/dd/yyyy", "%m/%d/%Y", "%m/%d/%y", "%d/%m/%Y", "%d/%m/%y", "%d-%m-%Y", 
                          "%d-%m-%y", "%m-%d-%Y", "%m-%d-%y", "%Y-%m-%d", "%f/%e/%Y", "%f/%e/%y", "%e/%f/%Y", "%e/%f/%y", 
                          "%f-%e-%Y", "%f-%e-%y", "%e-%f-%Y", "%e-%f-%y", "%b %e, %Y", "%B %e, %Y", "%b %d, %Y", "%B %d, %Y", 
-                         "%Y-%m-%d %H:%M:%S", "%H:%M:%S", "%Y-%m-%d %I:%M:%S %p","%b %Y", "%Y-%m-%d %I:%M:%S %p", "%b %d, %Y", "%b %Y"]
+                         "%Y-%m-%d %H:%M:%S", "%H:%M:%S", "%Y-%m-%d %I:%M:%S %p","%b %Y", "%Y-%m-%d %I:%M:%S %p", "%b %d, %Y", "%b %Y"]):
 
     for date_format in list_date_formats:
         succeed = True
