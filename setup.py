fe4f5f6ae2c0acd7933dcfc926957675d3786810
Ventura-Traveset <albert.ventura@maticapartners.com>
2019-10-15 11:37:18 +0200

    change folders

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DateFormatExtractor",
-    version="0.0.1",
+    name="dateFormatExtractor",
+    packages = ["dateFormatExtractor"],
+    version="1.0.2",
     author="Albert Ventura-Traveset",
     author_email="albertoventuratraveset@hotmail.com",
     description="This package can extract a specific date format from an iterable object (list, pandas column, etc)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/VenturaDev/DateExtractorRepo",
-    packages=setuptools.find_packages(),
     install_requires=['datetime'],
     classifiers=[
         "Programming Language :: Python :: 3",
